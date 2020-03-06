d2b3de90295a55d5acb5593ac9e1b853f6d817d9
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-03-05 21:38:30 -0600

    Fixes & Changes but for 1.15

--- src/main/java/com/bagel/bambootiful/core/registry/BambooBlockData.java
+++ src/main/java/com/bagel/bambootiful/core/registry/BambooBlockData.java
@@ -2,8 +2,6 @@ package com.bagel.bambootiful.core.registry;
 
 import com.bagel.bambootiful.core.util.DataUtils;
 
-import net.minecraftforge.fml.ModList;
-
 public class BambooBlockData {
 	public static void registerCompostables() {
 		//DataUtils.registerCompostable(0.3F, BambooBlocks.DRIED_BAMBOO.get());
@@ -11,15 +9,28 @@ public class BambooBlockData {
 	
 	public static void registerFlammables() {
 		DataUtils.registerFlammable(BambooBlocks.BAMBOO_PLANKS.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.BAMBOO_PILLAR.get(), 5, 20);
 		DataUtils.registerFlammable(BambooBlocks.BAMBOO_SLAB.get(), 5, 20);
 		DataUtils.registerFlammable(BambooBlocks.BAMBOO_STAIRS.get(), 5, 20);
 		DataUtils.registerFlammable(BambooBlocks.BAMBOO_FENCE.get(), 5, 20);
 		DataUtils.registerFlammable(BambooBlocks.BAMBOO_FENCE_GATE.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.VERTICAL_BAMBOO_PLANKS.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.BAMBOO_VERTICAL_SLAB.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.BAMBOO_BOOKSHELF.get(), 5, 20);
 		
-		if(ModList.get().isLoaded("quark")) {
-			DataUtils.registerFlammable(BambooBlocks.VERTICAL_BAMBOO_PLANKS.get(), 5, 20);
-			DataUtils.registerFlammable(BambooBlocks.BAMBOO_VERTICAL_SLAB.get(), 5, 20);
-			DataUtils.registerFlammable(BambooBlocks.BAMBOO_BOOKSHELF.get(), 5, 20);
-		}
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_PLANKS.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_PILLAR.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_SLAB.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_STAIRS.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_FENCE.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_FENCE_GATE.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.VERTICAL_DRIED_BAMBOO_PLANKS.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_VERTICAL_SLAB.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.DRIED_BAMBOO_BOOKSHELF.get(), 5, 20);
+		
+		DataUtils.registerFlammable(BambooBlocks.THATCH.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.THATCH_SLAB.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.THATCH_STAIRS.get(), 5, 20);
+		DataUtils.registerFlammable(BambooBlocks.THATCH_VERTICAL_SLAB.get(), 5, 20);
 	}
 }
